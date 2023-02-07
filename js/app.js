1f078d886e72135b6ba34b16fede07dcb1012968
unknown <biancapeleao@gmail.com>
2023-02-07 14:40:00 -0300

    finished

--- js/app.js
+++ js/app.js
@@ -4,22 +4,10 @@ const calcular = document.getElementById('calcular')
 
 calcular.addEventListener('click', somar)
 
-/*function somar() {
-    const numero1 = document.getElementById('numero1').value
-    const numero2 = document.getElementById('numero2').value
-    const formulario = document.getElementById('formulario')
-     const resultado = Number(numero1) + Number(numero2)/2
-    document.getElementById('resultado1').value = Number(numero1) + Number(numero2)
-
-    formulario.classList.add('sombra-vermelha')
-}*/
-const nota1 = document.getElementById('nota1').value
-const nota2 = document.getElementById('nota2').value
-const teste = document.getElementById('teste')
 
 function somar() {
-    const nota1 = document.getElementById('nota1').value
-    const nota2 = document.getElementById('nota2').value
+    const nota1 = document.getElementById('nota1').value.replace(',', '.')
+    const nota2 = document.getElementById('nota2').value.replace(',', '.')
     const formulario = document.getElementById('formulario')
 
     const resultado = (Number(nota1) + Number(nota2)) / 2
@@ -30,14 +18,45 @@ function somar() {
 
     document.getElementById('situacao').value = resultado
 
+    if (nota1 == '' || nota2 == '') {
+        document.getElementById('situacao').value = 'Entradas vazias!'
 
-    if (nota1 >= 5) {
+    } else if (
+        isNaN(nota1) || isNaN(nota2)
+    ) {
+        document.getElementById('situacao').value = 'Digite números'
+
+    } else if (
+        nota1 > 10 || nota2 > 10 || nota1 < 0 || nota2 < 0
+    ) {
+        document.getElementById('situacao').value = 'Notas só até 10!'
+    }
+
+    else {
+        if (resultado >= 5) {
+            document.getElementById('situacao').value = 'Aprovado!'
+            inputSituacao.classList.add('notaAzul')
+            inputSituacao.classList.remove('notaVermelha')
+            formulario.classList.add('aprovado')
+            formulario.classList.remove('reprovado')
+        } else {
+            document.getElementById('situacao').value = 'Reprovado!'
+            inputSituacao.classList.add('notaVermelha')
+            inputSituacao.classList.remove('notaAzul')
+            formulario.classList.add('reprovado')
+            formulario.classList.remove('aprovado')
+        }
+    }
+
+    if (nota1 >= 5 ) {
         inputNota1.classList.add('notaAzul')
         inputNota1.classList.remove('notaVermelha')
     } else {
         inputNota1.classList.remove('notaAzul')
         inputNota1.classList.add('notaVermelha')
     }
+  
+  
     if (nota2 >= 5) {
         inputNota2.classList.add('notaAzul')
         inputNota2.classList.remove('notaVermelha')
@@ -46,20 +65,8 @@ function somar() {
         inputNota2.classList.add('notaVermelha')
     }
 
-    if (resultado >= 5) {
-        document.getElementById('situacao').value = 'Aprovado!'
-        inputSituacao.classList.add('notaAzul')
-        inputSituacao.classList.remove('notaVermelha')
-        formulario.classList.add('aprovado')
-        formulario.classList.remove('reprovado')
-    } else {
-        document.getElementById('situacao').value = 'Reprovado!'
-        inputSituacao.classList.add('notaVermelha')
-        inputSituacao.classList.remove('notaAzul')
-        formulario.classList.add('reprovado')
-        formulario.classList.remove('aprovado')
-    }
 }
+
 
 
 
